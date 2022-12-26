e400fc33bd946a8568d92ab00f382a4e94c6c455
Swanand01 <75439077+Swanand01@users.noreply.github.com>
2022-12-26 22:50:17 +0530

    URLs and Views

--- book_recommendation/core/urls.py
+++ book_recommendation/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import recommend_book
+from .views import recommend_book, book_title_autocomplete
 
 urlpatterns = [
-    path('recommend-book/', recommend_book, name='recommend_book')
+    path('recommend-book/<str:book_name>',
+         recommend_book, name='recommend_book'),
+    path('autocomplete/<str:book_name>',
+         book_title_autocomplete, name='book_title_autocomplete')
 ]
--- book_recommendation/core/views.py
+++ book_recommendation/core/views.py
@@ -1,9 +1,58 @@
+import os
+import pickle
+import numpy as np
+
+from django.conf import settings
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Book
+
+core_path = os.path.join(settings.BASE_DIR, "core/")
+
+pt = pickle.load(open(core_path + "pt.pkl", "rb"))
+books = pickle.load(open(core_path + "books.pkl", "rb"))
+similarity_scores = pickle.load(
+    open(core_path + "similarity_scores.pkl", "rb"))
+
 
 @api_view(["GET"])
-def recommend_book(request):
+def recommend_book(request, book_name):
+    if not book_name in pt.index:
+        return Response({
+            "success": False,
+            "book_name": book_name,
+            "error": "Book not found."
+        })
+
+    index = np.where(pt.index == book_name)[0][0]
+    similar_items = sorted(
+        list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True
+    )[1:6]
+
+    data = []
+    for i in similar_items:
+        temp_df = books[books["Book-Title"] == pt.index[i[0]]]
+        item = {
+            "title": temp_df.drop_duplicates("Book-Title")["Book-Title"].values[0],
+            "author": temp_df.drop_duplicates("Book-Title")["Book-Author"].values[0],
+            "cover_image": temp_df.drop_duplicates("Book-Title")["Image-URL-M"].values[0]
+        }
+        data.append(item)
+
     return Response({
-        "MSG": "OK"
+        "success": True,
+        "book_name": book_name,
+        "similar_books": data
+    })
+
+
+@api_view(["GET"])
+def book_title_autocomplete(request, book_name):
+    books = Book.objects.filter(
+        title__contains=book_name).values_list("title", flat=True)
+    return Response({
+        "book_name": book_name,
+        "suggestions": books
     })
