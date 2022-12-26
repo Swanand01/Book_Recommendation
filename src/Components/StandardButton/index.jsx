17d95dd6ac08fe329a027d5a141341496be359bc
Swanand01 <75439077+Swanand01@users.noreply.github.com>
2022-12-27 00:49:01 +0530

    Updates to StandardButon

--- src/Components/StandardButton/index.jsx
+++ src/Components/StandardButton/index.jsx
@@ -1,10 +1,11 @@
 import styles from "./styles.module.scss";
 
-export default function StandardButton({ text, onClick }) {
+export default function StandardButton({ text, onClick, disabled, style }) {
     return (
         <div
-            className={styles.standardButton}
-            onClick={onClick}
+            className={`${styles.standardButton} ${disabled && styles.disabled}`}
+            onClick={!disabled && onClick}
+            style={style}
         >
             {text}
         </div>
